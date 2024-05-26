addaf0de6b2de4d1be743f031c1af8d5f6f7343d
Andrew <andrewliu08@gmail.com>
2024-05-26 00:02:41 -0700

    post call transcript stuff

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 import os
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from retell import Retell
 from dotenv import load_dotenv
 
@@ -40,11 +41,27 @@ async def make_call(request: Request):
         from_number="+15597447125",
         to_number=phone_number,
         override_agent_id=agent_id,
-        retell_llm_dynamic_variables=dynamic_vars
+        retell_llm_dynamic_variables=dynamic_vars,
+        drop_call_if_machine_detected=True,
     )
 
-    # Return the phone number and agent ID as the response
-    return {"phone_number": phone_number, "agent_id": call.agent_id}
+    return JSONResponse(content=call.dict())
+
+
+@app.get("/getcall/{call_id}")
+async def get_call(call_id: str):
+    # Get the API key from environment variables
+    api_key = os.getenv("RETELL_KEY")
+
+    # Initialize the Retell client
+    client = Retell(api_key=api_key)
+
+    # Get the call
+    call = client.call.retrieve(call_id=call_id)
+    print(call)
+
+    return JSONResponse(content=call.dict())
+
 
 if __name__ == "__main__":
     import uvicorn
