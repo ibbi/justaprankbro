348b5fc1ff0907ce72da6900fb3ee41c921c6da7
Ibrahim Hashme <hi@ibbi.io>
2024-05-27 01:30:59 -0700

    corS

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://ibbi.io"],  # Add your production URL here
+    allow_origins=["http://localhost:3000", "https://justaprankbro.onrender.com"],  # Add your production URL here
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
