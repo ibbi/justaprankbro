79d478e25da4d6abd60b25124a2293c09d7bc1b2
Ibrahim Hashme <hi@ibbi.io>
2024-05-28 19:05:55 -0700

    Format

---
+++ server/app/api/endpoints/calls.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from retell import Retell
+
+from app.core.config import get_settings
+from app.schemas.requests import CallCreateRequest
+
+router = APIRouter()
+
+
+@router.post("/")
+async def make_call(request: CallCreateRequest):
+    phone_number = request.phone_number
+    agent_id = request.agent_id
+    dynamic_vars = request.dynamic_vars
+
+    api_key = get_settings().retell.api_key
+    print("boogabooga", api_key)
+    client = Retell(api_key=api_key)
+
+    if agent_id == "custom":
+        llm = client.llm.create(
+            general_prompt=dynamic_vars.get("general_prompt"),
+            begin_message=dynamic_vars.get("begin_message", ""),
+        )
+
+        agent = client.agent.create(
+            llm_websocket_url=llm.llm_websocket_url,
+            voice_id=dynamic_vars.get("voice_id"),
+            agent_name="Custom Agent",
+        )
+
+        agent_id = agent.agent_id
+
+    call = client.call.create(
+        from_number="+15597447125",
+        to_number=phone_number,
+        override_agent_id=agent_id,
+        retell_llm_dynamic_variables=dynamic_vars,
+        drop_call_if_machine_detected=True,
+    )
+
+    return JSONResponse(content=call.dict())
+
+
+@router.get("/{call_id}")
+async def get_call(call_id: str):
+    api_key = get_settings().retell.api_key
+    client = Retell(api_key=api_key)
+
+    call = client.call.retrieve(call_id=call_id)
+
+    return JSONResponse(content=call.dict())
