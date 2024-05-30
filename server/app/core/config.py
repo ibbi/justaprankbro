3ebba172994b4959da7f8ba2616811b880d4c7de
Ibrahim Hashme <hi@ibbi.io>
2024-05-30 12:43:23 -0700

    restrict cors

--- server/app/core/config.py
+++ server/app/core/config.py
@@ -31,7 +31,11 @@ class Security(BaseModel):
     refresh_token_expire_secs: int = 28 * 24 * 3600  # 28d
     password_bcrypt_rounds: int = 12
     allowed_hosts: list[str] = ["localhost", "127.0.0.1", "onrender.com"]
-    backend_cors_origins: list[AnyHttpUrl] = []
+    backend_cors_origins: list[AnyHttpUrl] = [
+        "http://localhost:3000",
+        "http://localhost:8001",
+        "https://prankring.com",
+    ]
 
 
 class Database(BaseModel):
