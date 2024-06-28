ee1ecd01f5890bedde508322f7095ed6912b664e
Ibrahim Hashme <hi@ibbi.io>
2024-06-28 17:59:33 +0100

    both tracksz

--- server/app/api/endpoints/webhooks.py
+++ server/app/api/endpoints/webhooks.py
@@ -132,7 +132,9 @@ async def twilio_voice_webhook(
         response = VoiceResponse()
         # Start streaming to our backend
         start = response.start()
-        start.stream(url=f"wss://{request.headers['host']}/stream/twilio")
+        start.stream(
+            url=f"wss://{request.headers['host']}/stream/twilio", track="both_tracks"
+        )
 
         # Connect to Retell
         connect = response.connect()
