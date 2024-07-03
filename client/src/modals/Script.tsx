c77309bd1b0040dff608018decfdbddb68fd6817
Waj <agentwaj@gmail.com>
2024-07-03 18:15:00 +0100

    Fix leaky click on ScriptModal

--- client/src/modals/Script.tsx
+++ client/src/modals/Script.tsx
@@ -21,7 +21,7 @@ interface ScriptModalProps {
   onSubmit: (
     phoneNumber: string,
     scriptId: number,
-    dynamicVars: Record<string, string>
+    dynamicVars: Record<string, string>,
   ) => void;
   onSignUp: () => void;
   onClickPay: () => void;
@@ -42,7 +42,7 @@ const ScriptModal: React.FC<ScriptModalProps> = ({
   const [isPhoneInvalid, setIsPhoneInvalid] = useState(false);
 
   const handleInputChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) => {
     const { name, value } = e.target;
     setDynamicVars((prevVars) => ({ ...prevVars, [name]: value }));
@@ -74,8 +74,6 @@ const ScriptModal: React.FC<ScriptModalProps> = ({
 
   const isInvalidPurchase = () => !user || user.balance < (script?.cost || 1);
 
-  if (!script) return null;
-
   return (
     <Modal
       isOpen={!!script}
@@ -83,96 +81,98 @@ const ScriptModal: React.FC<ScriptModalProps> = ({
       className="dark"
       size={isMobile() ? "full" : "xl"}
     >
-      <WrapperWithHeader title={script.title}>
-        <ModalBody>
-          <PhoneInput
-            value={phoneNumber}
-            onChange={handlePhoneChange}
-            isInvalid={isPhoneInvalid}
-          />
-          <p className="font-bold">
-            (Optional) add personal details to freak them out!
-          </p>
-          <div className="grid grid-cols-2 gap-4">
-            {script.fields
-              .sort((a, b) => {
-                if (
-                  a.textbox_type === "textarea" &&
-                  b.textbox_type !== "textarea"
-                )
-                  return 1;
-                if (
-                  a.textbox_type !== "textarea" &&
-                  b.textbox_type === "textarea"
-                )
-                  return -1;
-                return 0;
-              })
-              .map((field) => {
-                if (field.textbox_type === "textarea") {
-                  return (
-                    <div key={field.variable_name} className="col-span-2">
-                      <Textarea
-                        label={field.form_label}
-                        name={field.variable_name}
-                        value={dynamicVars[field.variable_name] || ""}
-                        onChange={handleInputChange}
-                        className="w-full"
-                      />
-                    </div>
-                  );
-                } else {
-                  return (
-                    <div key={field.variable_name} className="col-span-1">
-                      <Input
-                        label={field.form_label}
-                        name={field.variable_name}
-                        value={dynamicVars[field.variable_name] || ""}
-                        onChange={handleInputChange}
-                        className="w-full"
-                      />
-                    </div>
-                  );
-                }
-              })}
-          </div>
+      {!script ? null : (
+        <WrapperWithHeader title={script.title}>
+          <ModalBody>
+            <PhoneInput
+              value={phoneNumber}
+              onChange={handlePhoneChange}
+              isInvalid={isPhoneInvalid}
+            />
+            <p className="font-bold">
+              (Optional) add personal details to freak them out!
+            </p>
+            <div className="grid grid-cols-2 gap-4">
+              {script.fields
+                .sort((a, b) => {
+                  if (
+                    a.textbox_type === "textarea" &&
+                    b.textbox_type !== "textarea"
+                  )
+                    return 1;
+                  if (
+                    a.textbox_type !== "textarea" &&
+                    b.textbox_type === "textarea"
+                  )
+                    return -1;
+                  return 0;
+                })
+                .map((field) => {
+                  if (field.textbox_type === "textarea") {
+                    return (
+                      <div key={field.variable_name} className="col-span-2">
+                        <Textarea
+                          label={field.form_label}
+                          name={field.variable_name}
+                          value={dynamicVars[field.variable_name] || ""}
+                          onChange={handleInputChange}
+                          className="w-full"
+                        />
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div key={field.variable_name} className="col-span-1">
+                        <Input
+                          label={field.form_label}
+                          name={field.variable_name}
+                          value={dynamicVars[field.variable_name] || ""}
+                          onChange={handleInputChange}
+                          className="w-full"
+                        />
+                      </div>
+                    );
+                  }
+                })}
+            </div>
 
-          <Button
-            color="success"
-            onPress={handleSubmit}
-            isDisabled={isInvalidPurchase()}
-            startContent={<PhoneIcon />}
-            className="mt-4"
-          >
-            <p className="text-white">Make the prank call!</p>
-          </Button>
-        </ModalBody>
-        {isInvalidPurchase() && (
-          <ModalFooter>
-            {!user ? (
-              <p>
-                Want a free call?{" "}
-                <span
-                  className="text-primary underline cursor-pointer"
-                  onClick={onSignUp}
-                >
-                  Sign up!
-                </span>
-              </p>
-            ) : (
-              <p>
-                You're broke!{" "}
-                <span
-                  className="text-primary underline cursor-pointer"
-                  onClick={onClickPay}
-                >
-                  Buy some credits!
-                </span>
-              </p>
-            )}
-          </ModalFooter>
-        )}
-      </WrapperWithHeader>
+            <Button
+              color="success"
+              onPress={handleSubmit}
+              isDisabled={isInvalidPurchase()}
+              startContent={<PhoneIcon />}
+              className="mt-4"
+            >
+              <p className="text-white">Make the prank call!</p>
+            </Button>
+          </ModalBody>
+          {isInvalidPurchase() && (
+            <ModalFooter>
+              {!user ? (
+                <p>
+                  Want a free call?{" "}
+                  <span
+                    className="text-primary underline cursor-pointer"
+                    onClick={onSignUp}
+                  >
+                    Sign up!
+                  </span>
+                </p>
+              ) : (
+                <p>
+                  You're broke!{" "}
+                  <span
+                    className="text-primary underline cursor-pointer"
+                    onClick={onClickPay}
+                  >
+                    Buy some credits!
+                  </span>
+                </p>
+              )}
+            </ModalFooter>
+          )}
+        </WrapperWithHeader>
+      )}
     </Modal>
   );
 };
