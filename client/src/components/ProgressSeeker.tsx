9df0b2acf2cadcaec27b766c31ee99dda2252e23
Waj <agentwaj@gmail.com>
2024-07-03 16:39:01 +0100

    Use flex for seeker width

--- client/src/components/ProgressSeeker.tsx
+++ client/src/components/ProgressSeeker.tsx
@@ -13,7 +13,7 @@ const ProgressSeeker = ({ progress, isPlaying, handleSeek }: Props) => {
 
       {isPlaying && (
         <div
-          className="ml-2 w-28 h-2 bg-white rounded hover:bg-slate-200"
+          className="ml-2 mr-1 flex-1 h-2 bg-white rounded hover:bg-slate-200"
           onClick={handleSeek}
         >
           <div
